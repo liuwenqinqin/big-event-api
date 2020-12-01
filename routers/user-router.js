b99b81aa746afefca54ef265f0336e7cee591a0e
lwq <15565680981@163.com>
2020-12-01 16:56:11 +0800

    l5 更新头像

--- routers/user-router.js
+++ routers/user-router.js
@@ -25,9 +25,52 @@ if(ret&&ret.length >0) {
  res.send('userinfo')
 })
 
+// 更新密码
+router.post('/updatepwd', async(req,res) =>{
+let id = req.user.id
+let params = req.body
+params.oldPwd = utility.md5(params.oldPwd)
+params.newPwd = utility.md5(params.newPwd)
+let sql = 'update myuser set password = ? where id = ? and password = ?'
+let ret = await db.operateDb(sql, [params.newPwd,id,params.oldPwd])
+// 3、返回前端结果
+if (ret && ret.affectedRows > 0) {
+    // 修改成功
+    res.json({
+      status: 0,
+      message: '重置密码成功'
+    })
+  } else {
+    // 修改失败
+    res.json({
+      status: 1,
+      message: '重置密码失败'
+    })
+  }
+})
 
-
-
+// 更新头像
+router.post('/update/avatar', async(req,res) =>{
+    let id = req.user.id
+    let params = req.body
+    let sql = 'update myuser set user_pic = ? where id = ?'
+    let ret = await db.operateDb(sql, [params.avatar,id])
+    // 3、返回前端结果
+    if (ret && ret.affectedRows > 0) {
+        // 修改成功
+        res.json({
+          status: 0,
+          message: '更新头像成功'
+        })
+      } else {
+        // 修改失败
+        res.json({
+          status: 1,
+          message: '更新头像失败'
+        })
+      }
+    })
+    
 
 
 
